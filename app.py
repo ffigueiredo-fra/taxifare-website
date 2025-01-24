ff9b1b92c9439f8c2b0d680b1196ef9ce1c79f96
Filipe Figueiredo <ffigueiredo.fra@users.noreply.github.com>
2025-01-24 14:42:28 +0100

    My first website

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import requests
+
+'''
+# TaxiFareModel front
+'''
+
+st.markdown('''
+Remember that there are several ways to output content into your web page...
+
+Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+''')
+
+'''
+## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+
+1. Let's ask for:
+- date and time
+- pickup longitude
+- pickup latitude
+- dropoff longitude
+- dropoff latitude
+- passenger count
+'''
+
+'''
+## Once we have these, let's call our API in order to retrieve a prediction
+
+See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+
+🤔 How could we call our API ? Off course... The `requests` package 💡
+'''
+
+url = 'https://qonto-973308060059.europe-west1.run.app/predict'
+
+if url == 'https://taxifare.lewagon.ai/predict':
+
+    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+
+params = {'pickup_datetime': '2014-07-06 19:18:00',
+          'pickup_longitude': -73.950655,
+          'pickup_latitude': 40.783282,
+          'dropoff_longitude': -73.984365,
+          'dropoff_latitude': 40.769802,
+          'passenger_count': 2
+        }
+
+response = requests.get(
+    url=url,
+    params=params,
+).json()
+
+st.write(response)
